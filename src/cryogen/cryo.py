e660be490dfe157d1cd0f4b99f308c88ca4d59a9
banteg <4562643+banteg@users.noreply.github.com>
2023-08-21 14:56:59 +0400

    test: range utils

--- src/cryogen/cryo.py
+++ src/cryogen/cryo.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 
 def extract_range(file: str) -> range:
-    start_block, stop_block = re.findall(r"\d{7,}", Path(file).stem)
+    start_block, stop_block = re.findall(r"\d+", Path(file).stem)
     return range(int(start_block), int(stop_block) + 1)
 
 
-def replace_range(file: str, r: range):
-    name = re.sub(r"\d+_to_\d+", f"{r.start:08d}_to_{r.stop:08d}", Path(file))
-    return Path(file).with_stem(name)
+def replace_range(file: str, r: range) -> str:
+    name = re.sub(r"\d+_to_\d+", f"{r.start:08d}_to_{r.stop:08d}", Path(file).stem)
+    return str(Path(file).with_stem(name))
