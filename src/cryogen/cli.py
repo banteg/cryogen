6128a96d954707e1a050d62cf5805544d6d88b34
banteg <4562643+banteg@users.noreply.github.com>
2023-08-24 11:28:13 +0400

    feat: add consolidate command

--- src/cryogen/cli.py
+++ src/cryogen/cli.py
@@ -3,9 +3,11 @@ from typer import Typer, Argument, Option, prompt
 from pathlib import Path
 from click import Choice
 from enum import Enum
-from cryogen.consolidate import find_gaps
-from cryogen.utils import extract_range, parse_blocks
+from cryogen.consolidate import find_gaps, combine_ranges
+from cryogen.utils import extract_range, parse_blocks, replace_range
+from cryogen.parquet import merge_parquets, parquet_info
 from cryogen.constants import FAR_AWAY_BLOCK
+from rich import print
 
 app = Typer()
 
@@ -41,6 +43,36 @@ def collect(
     ranges = [extract_range(file) for file in dataset_dir.glob("*.parquet")]
     gaps = find_gaps(ranges)
     print(gaps)
+
+
+@app.command()
+def consolidate(
+    dataset: Dataset,
+    data_dir: Annotated[Path, Option(envvar="CRYO_DATA_DIR")],
+    suffix: str = "out",
+):
+    print(dataset.value, data_dir)
+    dataset_dir = data_dir / dataset.value
+    output_dir = data_dir / (dataset.value + f"_{suffix}")
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    print(dataset_dir)
+    info = parquet_info(dataset_dir)
+    print(info)
+
+    sample_name = next(dataset_dir.glob("*.parquet"))
+    ranges = [extract_range(file) for file in dataset_dir.glob("*.parquet")]
+    combined = combine_ranges(ranges)
+
+    for r in combined:
+        # print(f"consolidating {r} from {len(combined[r])} files")
+        output_file = output_dir / replace_range(sample_name, r).name
+        input_files = [replace_range(sample_name, sub) for sub in combined[r]]
+        # print(output_file)
+        merge_parquets(input_files, output_file)
+
+    info_out = parquet_info(output_dir)
+    print(info_out)
 
 
 if __name__ == "__main__":
